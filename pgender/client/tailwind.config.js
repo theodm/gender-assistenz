b95f7f52193dd5ed511a6eb1edc281cb3c4f10ef
Theodor Diesner-Mayer <theo.dm94@gmail.com>
2021-09-15 19:28:52 +0200

    Aufräumen #1

--- pgender/client/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-// tailwind.config.js
-module.exports = {
-//   mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,svelte}"],
-  // specify other options here
-};
--- py3.9/corenlp/corenlp_down.py
+++
@@ -1,5 +0,0 @@
-import stanza
-
-stanza.install_corenlp(dir="C:\\CoreNLP")
-
-stanza.download_corenlp_models(model='german', version='4.2.2', dir="C:\\CoreNLP")
